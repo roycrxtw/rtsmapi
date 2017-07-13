36aa071a0c17269468b03b8f0993eda0da486729
Roy Lu <royvbtw@gmail.com>
2017-07-13 21:25:47 +0800

    edit deploy script

--- deploy/after-install.sh
+++ deploy/after-install.sh
@@ -1,11 +1,11 @@
 # deploy/after-install.sh
 #!/bin/bash
 source /home/ec2-user/.bash_profile
+cd /var/node/rtsmapi
 npm cache clean
 npm install
 sudo chown -R ec2-user /var/node/rtsmapi
 sudo chgrp -R ec2-user /var/node/rtsmapi
 chmod -R 755 /var/node/rtsmapi
-cd /var/node/rtsmapi
 sudo mkdir log
 pm2 start /var/node/rtsmapi/index.js --name='rtsmapi'
